d6606c20d9aad439dd5c55fcfa5a1a032cc42b95
Maciej Oczko <maciek.oczko@polidea.com>
2014-01-13 10:01:34 +0100

    Initial Commit

---
+++ MOCollectionUtilities/MOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MOAppDelegate.h
+//  MOCollectionUtilities
+//
+//  Created by Maciej Oczko on 13/01/14.
+//  Copyright (c) 2014 Maciej Oczko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MOCollectionUtilities/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MOCollectionUtilities
+//
+//  Created by Maciej Oczko on 13/01/14.
+//  Copyright (c) 2014 Maciej Oczko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MOAppDelegate class]));
+    }
+}
